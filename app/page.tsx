6300f5eb2005b2f611fdee37cc86aa9446cecd1c
zhyantao <yantao.z@outlook.com>
2024-12-15 13:55:08 +0800

    rename repo

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/callgraphs-frontend/next.svg"
+          src="/callgraphs-generator/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -32,7 +32,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/callgraphs-frontend/vercel.svg"
+              src="/callgraphs-generator/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -58,7 +58,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/callgraphs-frontend/file.svg"
+            src="/callgraphs-generator/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -73,7 +73,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/callgraphs-frontend/window.svg"
+            src="/callgraphs-generator/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -88,7 +88,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/callgraphs-frontend/globe.svg"
+            src="/callgraphs-generator/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
